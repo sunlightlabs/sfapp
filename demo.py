a9f3ebd278de2bbe8a8cdb27afafed99042fd6c0
Jeremy Carbaugh <jcarbaugh@gmail.com>
2012-03-28 17:57:42 -0400

    initial commit

---
+++ demo.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+from django.conf.urls.defaults import *
+
+PWD = os.path.abspath(os.path.dirname(__file__))
+
+HOST = '127.0.0.1:8000'
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+INSTALLED_APPS = (
+	'django.contrib.staticfiles',
+	'sfapp',
+)
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PWD, 'static-root')
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+ROOT_URLCONF = 'demo'
+
+
+# configure urls
+urlpatterns = patterns('django.views.generic.simple', 
+    (r'^$', 'direct_to_template', {'template': 'sfapp/_demo.html'}),
+)
+
+def bootstrap():
+
+	from django.core.management import call_command
+
+	# add current directory to path
+	sys.path.append(PWD)
+
+	os.environ['DJANGO_SETTINGS_MODULE'] = 'demo'
+	
+	call_command('runserver', HOST)
+
+
+if __name__ == '__main__':
+	bootstrap()
