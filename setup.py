dee5b7aef0335c007ecba31ab0dd762a8e8e9cde
Jeremy Carbaugh <jcarbaugh@gmail.com>
2012-04-09 15:49:32 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+from sfapp import __version__
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+readme = f.read()
+f.close()
+
+setup(
+    name='sfapp',
+    version=__version__,
+    description='Django app for Sunlight Foundation mini-app wrapper',
+    long_description=readme,
+    author='Jeremy Carbaugh',
+    author_email='jcarbaugh@sunlightfoundation.com',
+    url='http://github.com/sunlightlabs/sfapp/',
+    packages=find_packages(),
+    package_data={
+        'sfapp': [
+            'static/*/*',
+            'templates/*/*',
+        ]
+    },
+    license='BSD License',
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
