d3f8597a4671f9f87dc0108a8b47e1224c2eadbe
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-01-17 17:31:20 -0500

    Bootstrap 3 success

--- demo.py
+++ demo.py
@@ -2,50 +2,57 @@
 import os
 import sys
 
-from django.conf.urls import *
+from django.conf import settings, global_settings
+from django.conf.urls import patterns
+from django.core.management import call_command
 
 PWD = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(PWD)
+
 
 try:
     HOST = sys.argv[1]
 except IndexError:
     HOST = '127.0.0.1:8000'
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
-INSTALLED_APPS = (
-    'django.contrib.staticfiles',
-    'sfapp',
-)
+def index(request):
+    from django.shortcuts import render
+    return render(request, 'sfapp/_demo.html')
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PWD, 'static-root')
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
-ROOT_URLCONF = 'demo'
-
-
-# configure urls
-urlpatterns = patterns('django.views.generic.simple',
-    (r'^$', 'direct_to_template', {'template': 'sfapp/_demo.html'}),
+urlpatterns = patterns('',
+    (r'^$', index),
 )
 
 
-def bootstrap():
+class DemoSettings(object):
 
-    from django.core.management import call_command
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
 
-    # add current directory to path
-    sys.path.append(PWD)
+    SECRET_KEY = 'Wvr32LrQ2mchbA7SUdBiy5Vy/UoauUJ/m6tlzP1woIE='
 
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'demo'
+    INSTALLED_APPS = (
+        'django.contrib.staticfiles',
+        'sfapp',
+    )
 
-    call_command('runserver', HOST)
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(PWD, 'static-root')
+    STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
+
+    ROOT_URLCONF = 'demo'
+
+    def __getattr__(self, name):
+        return getattr(global_settings, name)
 
 
 if __name__ == '__main__':
-    bootstrap()
+
+    demo_settings = DemoSettings()
+    settings.configure(demo_settings)
+
+    call_command('runserver', HOST)
